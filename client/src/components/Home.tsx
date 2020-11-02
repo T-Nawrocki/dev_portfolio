93051034e2754771798ce9ee02c3769e4481c448
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-02 15:06:17 +0000

    basic home page complete

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import NavButton from './NavButton';
  
 const Home: React.FunctionComponent = () => {
   return ( 
@@ -6,7 +7,9 @@ const Home: React.FunctionComponent = () => {
       <h1>Tomasz Nawrocki</h1>
       <h2>Software Developer</h2>
       <nav id="home-nav">
-        
+        <NavButton destination="/about-me" text="About Me" />
+        <NavButton destination="/portfolio" text="Portfolio" />
+        <NavButton destination="/contact" text="Contact" />
       </nav>
     </div>
    );
