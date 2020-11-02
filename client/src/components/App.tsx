bd5dda38999755258a958336da8dcd9e86cba393
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-02 14:53:20 +0000

    router added

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../styles/App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            {/* <Route path="/about-me" component={About} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} /> */}
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+ 
+const Home: React.FunctionComponent = () => {
+  return ( 
+    <div id="home">
+
+    </div>
+   );
+}
+ 
+export default Home;
